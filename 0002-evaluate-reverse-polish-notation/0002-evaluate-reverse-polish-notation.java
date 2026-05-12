647c388d18f84bab1625fa51790408db0a2fe3a8
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-05-13 02:10:23 +0530

    Time: 8 ms (10.92%), Space: 44.9 MB (94.54%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0002-evaluate-reverse-polish-notation/0002-evaluate-reverse-polish-notation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        for(String ch : tokens) {
+            if(ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/")) {
+                int b = st.pop();
+                int a = st.pop();
+                if(ch.equals("+")) st.push(a + b);
+                if(ch.equals("-")) st.push(a - b);
+                if(ch.equals("*")) st.push(a * b);
+                if(ch.equals("/")) st.push(a / b);
+            }
+            else st.push(Integer.parseInt(ch));
+        }
+        return st.peek();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
