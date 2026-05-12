2498c6564f8526034f7380fdcaaa4e70561f1819
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-05-12 12:47:37 +0530

    Time: 23 ms (88.46%), Space: 113.8 MB (79.81%) - LeetHub - Time: O(?), Space: O(?)

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -1,30 +1,14 @@
 class Solution {
-    private boolean canFinish(int[][] tasks, int mid) {
-        int n = tasks.length;
-        for(int i = 0; i < n; i++) {
-            if(mid < tasks[i][1]) return false;
-            mid -= tasks[i][0];
-        }
-        return true;
-    }
     public int minimumEffort(int[][] tasks) {
         int n = tasks.length;
         Arrays.sort(tasks, (a, b) -> (b[1] - b[0]) - (a[1] - a[0]));
-        int l = 1;
-        int h = 0;
-        for(int i = 0; i < n; i++) {
-            l = Math.max(l, tasks[i][1]);
-            h += tasks[i][1];
-        }
         int ans = 0;
-        while(l <= h) {
-            int mid = l + (h - l) / 2;
-            if(canFinish(tasks, mid)) {
-                ans = mid;
-                h = mid - 1;
-            }
-            else l = mid + 1;
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            ans = Math.max(ans, tasks[i][1] + sum);
+            sum += tasks[i][0];
         }
+        
         return ans;
     }
 }
