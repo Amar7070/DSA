cd1c3197da2fdc091213468c524ad9a4b6317d45
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-05-23 01:34:37 +0530

    Time: 0 ms (100%), Space: 44 MB (26.59%) - LeetHub - Time: O(log n), Space: O(1)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,33 +1,20 @@
 class Solution {
-    public int binarySearch(int[] a,int l,int h,int t){
-        while(l<=h){
-            int m=l+(h-l)/2;
-            if(a[m]==t) return m;
-            else if(a[m]<t) l=m+1;
-            else h=m-1;
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int h = nums.length - 1;
+        while(l <= h) {
+            int mid = l + (h - l) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[l] <= nums[mid]) {
+                if(target >= nums[l] && target <= nums[mid]) h = mid - 1;
+                else l = mid + 1;
+            }
+            else {
+                if(target >= nums[mid] && target <= nums[h]) l = mid + 1;
+                else h = mid - 1;
+            }
         }
         return -1;
-    }
-    public int search(int[] nums, int target) {
-        int l=0;
-        int h=nums.length-1;
-        int e=h;
-        while(l<h){
-            int m=l+(h-l)/2;
-            if(nums[m]>nums[h]){
-                l=m+1;
-            }
-            else{
-                h=m;
-            }
-        }
-        if(nums[l]==target) return l;
-        if(target>nums[e] && l-1>=0){
-            return binarySearch(nums,0,l-1,target);
-        }
-        else{
-            return binarySearch(nums,l,e,target);
-        }
     }
 }
 
