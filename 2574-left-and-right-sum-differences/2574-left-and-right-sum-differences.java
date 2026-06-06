bbff1cb0f7d459d476f06fc8b74b00d8366b1645
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-06-07 00:59:10 +0530

    Time: 2 ms (98.84%), Space: 46.6 MB (29.54%) - LeetHub - Time: O(?), Space: O(?)

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -6,7 +6,6 @@ class Solution {
         for(int i = n - 1; i >= 1; i--) {
             s += nums[i];
         }
-        System.out.println(s);
         int p = 0;
         for(int i = 0; i < n; i++) {
             ans[i] = Math.abs(s - p);
