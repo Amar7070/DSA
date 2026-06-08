a430a3c0c77f4fbd2c5b963c77ff61dba15ef0fb
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-06-08 19:43:15 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.67%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xorVal = x ^ y;
+        int count = 0;
+        while(xorVal != 0) {
+            count += xorVal & 1;
+            xorVal >>= 1;
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
