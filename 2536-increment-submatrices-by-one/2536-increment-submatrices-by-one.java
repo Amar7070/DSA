ef16d9a883f5949afdb97d08aef289012a05f3e2
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-06-22 04:37:44 +0530

    Time: 11 ms (92.64%), Space: 81.5 MB (97.84%) - LeetHub - Time: O(n * m + n^2), Space: O(n^2)

---
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int ans[][] = new int[n][n];
+        for(int q[] : queries) {
+            int r1 = q[0];
+            int c1 = q[1];
+            int r2 = q[2];
+            int c2 = q[3];
+            ans[r1][c1] += 1;
+            if(c2 + 1 < n) ans[r1][c2 + 1] -= 1;
+            if(r2 + 1 < n) ans[r2 + 1][c1] -= 1;
+            if(c2 + 1 < n && r2 + 1 < n) ans[r2 + 1][c2 + 1] += 1;
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                ans[i][j] += ans[i][j - 1];
+            }
+        }
+
+        for(int j = 0; j < n; j++) {
+            for(int i = 1; i < n; i++) {
+                ans[i][j] += ans[i - 1][j];
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
