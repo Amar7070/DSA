c73b98cf6c5d1ecf928653580b6c4e45b5ccae7f
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-06-25 15:30:26 +0530

    Time: 9 ms (60.48%), Space: 48.5 MB (9.99%) - LeetHub - Time: O(n log m), Space: O(1)

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -7,20 +7,21 @@ class Solution {
         }
         while(l <= r) {
             int mid = l + (r - l) / 2;
-            if(isPossible(mid, piles) <= h) {
+            long total = totalHours(mid, piles);
+            if(total <= h) {
                 r = mid - 1;
             }
             else l = mid + 1;
         }
         return l;
     }
-    private int isPossible(int max, int[] piles) {
-        int hour = 0;
-        for(int pile : piles) {
-            hour += Math.ceil((double)pile / max);
-        }
-        return hour;
+    private long totalHours(int speed, int[] piles) {
+    long hours = 0;
+    for (int pile : piles) {
+        hours += (pile + speed - 1L) / speed;
     }
+    return hours;
+}
 }
 
 // Synced seamlessly with LeetHub Pro
