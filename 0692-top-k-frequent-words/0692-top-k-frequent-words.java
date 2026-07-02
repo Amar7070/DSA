4b56eb334a2ea0c94d6d7615640c40f8a5465cca
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-07-02 16:25:02 +0530

    Time: 7 ms (85.89%), Space: 45.8 MB (99.53%) - LeetHub - Time: O(?), Space: O(?)

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -13,16 +13,20 @@ class Solution {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
-            if (a.freq != b.freq) return b.freq - a.freq;
-            return (a.s).compareTo(b.s);
+            if (a.freq != b.freq) return a.freq - b.freq;
+            return (b.s).compareTo(a.s);
         });
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             pq.add(new Pair (entry.getKey(), entry.getValue()));
-        }
+            if (pq.size() > k)
+                pq.poll();
+            }
         List<String> ans = new ArrayList<>();
         while (k-- > 0) {
             ans.add(pq.poll().s);
+
         }
+        Collections.reverse(ans);
         return ans;
     }
 }
