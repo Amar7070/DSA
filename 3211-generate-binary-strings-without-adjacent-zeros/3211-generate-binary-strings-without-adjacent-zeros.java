efaaf9a3e6857501188390ff2875ec3c714a3fc5
Amar Sahani <151068364+Amar7070@users.noreply.github.com>
2026-07-07 16:46:42 +0530

    Time: 1 ms (100%), Space: 47.8 MB (23.89%) - LeetHub

---
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> validStrings(int n) {
+        List<String> ans = new ArrayList<>();
+        solve (new StringBuilder(), ans, n);
+        return ans;
+    }
+    private void solve (StringBuilder sb, List<String> ans, int n) {
+        if (sb.length() == n) {
+            ans.add(sb.toString());
+            return;
+        }
+
+        if (sb.isEmpty() || sb.charAt(sb.length() - 1) != '0') {
+            sb.append('0');
+            solve (sb, ans, n);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        sb.append('1');
+        solve (sb, ans, n);
+        sb.deleteCharAt(sb.length() - 1);
+    }  
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
