8cd7264133050010e0c1ddf2b91626ef20346e58
Amar Sahani <amarsahani0777@gmail.com>
2026-07-13 22:28:12 +0530

    Time: 4 ms (98.91%), Space: 47.9 MB (22.55%) - LeetHub - Time: O, Space: O(?)

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,20 +1,20 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
         int indeg[] = new int[n];
-        HashSet<String> set = new HashSet<>();
+        boolean[][] mat = new boolean[n][n];
         int ans = 0;
         for (int r[] : roads) {
             int u = r[0];
             int v = r[1];
-            set.add(u + " " + v);
-            set.add(v + " " + u);
+            mat[u][v] = true;
+            mat[v][u] = true;
             indeg[u]++;
             indeg[v]++;
         }
         for (int i = 0; i < n; i++) {
             for (int j =  i + 1; j < n; j++) {
                 int curr = indeg[i] + indeg[j];
-                if (set.contains(i + " " + j)) curr--;
+                if (mat[i][j]) curr--;
                 ans = Math.max(ans, curr);
             } 
         }
