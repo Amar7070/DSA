b90dc01972f5d164f2c263bd4f5c7d45c2325f76
Amar Sahani <amarsahani0777@gmail.com>
2026-07-19 01:14:04 +0530

    Time: 1 ms (100%), Space: 42.5 MB (100%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3993-maximum-value-of-an-alternating-sequence/3993-maximum-value-of-an-alternating-sequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumValue(int n, int s, int m) {
+        long ans = s;
+        if (n >= 2) {
+            ans += m;
+            n -= 2;
+        }
+        ans += 1L * (n / 2) * (m - 1);
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
