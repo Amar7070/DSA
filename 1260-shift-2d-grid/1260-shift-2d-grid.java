aa3c88d564a4dc3556c93bd4713d8d869225c3be
Amar Sahani <amarsahani0777@gmail.com>
2026-07-20 15:20:26 +0530

    Time: 5 ms (83.45%), Space: 47.4 MB (26.21%) - LeetHub - Time: O(n * m), Space: O(n * m)

--- 1260-shift-2d-grid/1260-shift-2d-grid.java
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -1,41 +1,29 @@
 class Solution {
-    private void reverse(int[] arr, int l, int r) {
-        while (l < r) {
-            int temp = arr[l];
-            arr[l] = arr[r];
-            arr[r] = temp;
-            l++;
-            r--;
-        }
-    } 
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         int n = grid.length;
         int m = grid[0].length;
-        
-        while (k-- > 0) {
-            int temp[] = new int[n];
-            int j = 0;
-            for (int i = 0; i < n; i++) {
-                reverse(grid[i], 0, m - 1);
-                reverse(grid[i], 1, m - 1);
-                temp[j++] = grid[i][0];
-            }
+        int total = n * m;
 
-            reverse(temp, 0, n - 1);
-            reverse(temp, 1, n - 1);
+        k %= total;
 
-            for (int i = 0; i < n; i++) {
-                grid[i][0] = temp[i];
-            }
+        int[][] res = new int[n][m];
+
+        for (int i = 0; i < total; i++) {
+            int newIndex = (i + k) % total;
+
+            res[newIndex / m][newIndex % m] = grid[i / m][i % m];
         }
+
         List<List<Integer>> ans = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            ans.add(new ArrayList<>());
+            List<Integer> row = new ArrayList<>();
             for (int j = 0; j < m; j++) {
-                ans.get(i).add(grid[i][j]);
+                row.add(res[i][j]);
             }
+            ans.add(row);
         }
+
         return ans;
     }
 }
