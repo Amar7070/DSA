0d4f80d7205b4aae49d6c70e978056fe6caaba22
Amar Sahani <amarsahani0777@gmail.com>
2026-07-23 11:26:17 +0530

    Time: 2 ms (8.01%), Space: 45.6 MB (5.7%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    HashMap<Integer, Integer> preMap;
+    HashMap<Integer, Integer> postMap;
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        int n = preorder.length;
+
+        preMap = new HashMap<>();
+        postMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            preMap.put (preorder[i], i);
+            postMap.put (postorder[i], i);
+        }
+
+        return solve (0, n - 1, preorder, postorder);
+    }
+
+    private TreeNode solve (int start, int end, int[] preorder, int[] postorder) {
+        if (start > end) return null;
+
+        TreeNode root = new TreeNode (preorder[start]);
+        if (start == end) return root;
+
+        int m = preMap.get(postorder[postMap.get(preorder[start]) - 1]);
+
+        root.left = solve (start + 1, m - 1, preorder, postorder);
+        root.right = solve (m, end, preorder, postorder);
+
+        return root;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
