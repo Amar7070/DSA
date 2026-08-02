6358535d020ef005a10f81ecda40a66ae4a8fc8b
Amar Sahani <amarsahani0777@gmail.com>
2026-08-03 03:32:21 +0530

    Time: 0 ms (100%), Space: 43.3 MB (37.02%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0877-stone-game/0877-stone-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    // private int solve (int s, int e, int[] piles) {
+    //     if (s == )
+    // }
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        // int ans = solve (0, n - 1, piles);
+        return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
