71d1c8f460e8c2048ffda70efe986d9076b6ecb3
Amar Sahani <amarsahani0777@gmail.com>
2026-08-07 21:07:30 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 909660-insertion-at-the-beginning-of-circular-linked-list-GFG/909660-insertion-at-the-beginning-of-circular-linked-list.java
@@ -0,0 +1,24 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtBeginning(Node tail, int key) {
+        // code here
+        Node temp = new Node(key);
+        temp.next = tail.next;
+        tail.next = temp;
+        return temp;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
