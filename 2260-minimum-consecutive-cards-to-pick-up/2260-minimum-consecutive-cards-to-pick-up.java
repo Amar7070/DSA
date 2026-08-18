986a015a6f26ec880d113480af318388e697d06d
Amar Sahani <amarsahani0777@gmail.com>
2026-08-19 02:06:58 +0530

    Time: 43 ms (69.34%), Space: 106.6 MB (35.21%) - LeetHub - Time: O(n), Space: O(n)

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -4,10 +4,13 @@ class Solution {
         int ans = Integer.MAX_VALUE;
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            if (map.containsKey(cards[i])) {
-                ans = Math.min (ans, i - map.get(cards[i]) + 1);
+            if (!map.containsKey(cards[i])) {
+                map.put(cards[i], i);
             }
-            map.put(cards[i], i);
+            else {
+                ans = Math.min (ans, i - map.get(cards[i]) + 1);
+                map.put(cards[i], i);
+            }
         }
         return (map.size() == n) ? -1 : ans;
     }
