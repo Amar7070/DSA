25c6de81c6dcf780254430948d7e45b38db4e450
Amar Sahani <amarsahani0777@gmail.com>
2026-09-14 00:22:47 +0530

    Time: 69 ms (32.95%), Space: 47.2 MB (40.53%) - LeetHub - Time: O(n^4), Space: O(n^2)

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,26 +1,43 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int n = img1.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int ans = 0;
 
+        List<int[]> ones1 = new ArrayList<>();
+        List<int[]> ones2 = new ArrayList<>();
+
+        // Store coordinates of 1s
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (img1[i][j] == 1) {
-                    for (int p = 0; p < n; p++) {
-                        for (int q = 0; q < n; q++) {
-                            if (img2[p][q] == 1) {
-                                int x = p - i;
-                                int y = q - j;
-                                int curr = x * 100 + y;
-                                map.put(curr, map.getOrDefault(curr, 0) + 1);
-                                ans = Math.max (ans, map.get(curr));
-                            }
-                        }
-                    }
+                    ones1.add(new int[]{i, j});
+                }
+
+                if (img2[i][j] == 1) {
+                    ones2.add(new int[]{i, j});
                 }
             }
         }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+
+        // Compare every 1 in img1 with every 1 in img2
+        for (int[] a : ones1) {
+            for (int[] b : ones2) {
+
+                int dx = b[0] - a[0];
+                int dy = b[1] - a[1];
+
+                // Encode (dx, dy) into one integer
+                int key = dx * 1000 + dy;
+
+                int count = map.getOrDefault(key, 0) + 1;
+                map.put(key, count);
+
+                ans = Math.max(ans, count);
+            }
+        }
+
         return ans;
     }
 }
