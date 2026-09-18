00415b59a7a6d1a24c01f3df7ca6c03e153603fc
Amar Sahani <amarsahani0777@gmail.com>
2026-09-19 01:59:26 +0530

    Time: 4 ms (95.41%), Space: 44.3 MB (98.79%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,11 @@
+import java.nio.file.*;
+class Solution {
+    public String simplifyPath(String path) {
+        Path p = Path.of(path);
+        return p.normalize().toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
